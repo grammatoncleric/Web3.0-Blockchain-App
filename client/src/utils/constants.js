65298c649447f985c9a5a55a21bc374f7b89662a
Augustine Okore <okey.okore@gmail.com>
2022-04-02 20:06:59 +0100

    check in smart contract deployment

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi; 
+export const contractAddress = '0xB022FBCa84A0Eaa961e6681952d7B3974Bc02c6A'
--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,22 +1,23 @@
-require("@nomiclabs/hardhat-waffle");
+//https://eth-ropsten.alchemyapi.io/v2/RELpN19GqEanFHFD8loBFQGmS4JKGY0c1
 
+require('@nomiclabs/hardhat-waffle');
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
+// Go to https://www.alchemyapi.io, sign up, create
+// a new App in its dashboard, and replace "KEY" with its key
+const ALCHEMY_API_KEY = "KEY";
 
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
+// Replace this private key with your Ropsten account private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Be aware of NEVER putting real Ether into testing accounts
+const ROPSTEN_PRIVATE_KEY = "0f12c87c19b0e0ee1389954b3380fc65c07d84a85723497c6f8791f040ead320";
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
-};
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: 'https://eth-ropsten.alchemyapi.io/v2/RELpN19GqEanFHFD8loBFQGmS4JKGY0c',
+      accounts: ['0f12c87c19b0e0ee1389954b3380fc65c07d84a85723497c6f8791f040ead320']
+    }
+  }
+}
